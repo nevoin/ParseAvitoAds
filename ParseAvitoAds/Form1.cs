50b258c30a236dcf157bec531ce6e1e399278d48
nevoin <fr_ord@mail.ru>
2019-11-10 19:01:19 +0300

    parse 1

--- ParseAvitoAds/Form1.cs
+++ ParseAvitoAds/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ParseAvitoAds
 {
@@ -15,6 +10,53 @@ namespace ParseAvitoAds
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		public static string GetHtml(string url)
+		{
+			string fullurl = url;
+
+			// create WebClient object
+			using (var webClient = new WebClient())
+			{
+				webClient.Encoding = System.Text.Encoding.UTF8;
+				try
+				{
+					return webClient.DownloadString(fullurl);
+				}
+				catch (Exception)
+				{
+					return "Some Error";
+				}
+			}
+		}
+
+		private void startButton_Click(object sender, EventArgs e)
+		{
+			string startUrl = "https://www.avito.ru/moskva?radius=5&geoCoords=55.733939038966945%2C37.6406529148446";
+			string parseStr1 = "data-item-url=\"";
+
+			//loading main Avito page with recent ads
+			startButton.Text = "require main avito page..";
+			Application.DoEvents();
+			string html1 = GetHtml(startUrl);
+			File.WriteAllText("html.txt", html1);
+			startButton.Text = "main avito page is loaded and save";
+			Application.DoEvents();
+
+			string adsUrl = "init";
+			int ind1, ind2 = 1;
+			string[] urls = new string[100];
+
+			//parsing ads urls from main page
+			while (adsUrl.Length > 0)
+			{
+				ind1 = html1.IndexOf(parseStr1);
+				html1 = html1.Substring(ind1+ parseStr1.Length, (html1.Length - ind1 - parseStr1.Length));
+				ind2 = html1.IndexOf("\"");
+				if (ind1 > 0 && ind2 > 0) adsUrl = html1.Substring(0, ind2);
+				MessageBox.Show(adsUrl);
+			}
 		}
 	}
 }
