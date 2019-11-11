c2fff6888a250522dc323913e42764c8079afbc7
nevoin <fr_ord@mail.ru>
2019-11-11 10:47:47 +0300

    good

--- ParseAvitoAds/Form1.Designer.cs
+++ ParseAvitoAds/Form1.Designer.cs
@@ -28,13 +28,34 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.startButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// startButton
+			// 
+			this.startButton.Location = new System.Drawing.Point(139, 191);
+			this.startButton.Name = "startButton";
+			this.startButton.Size = new System.Drawing.Size(333, 56);
+			this.startButton.TabIndex = 0;
+			this.startButton.Text = "Start";
+			this.startButton.UseVisualStyleBackColor = true;
+			this.startButton.Click += new System.EventHandler(this.startButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(622, 286);
+			this.Controls.Add(this.startButton);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button startButton;
 	}
 }
 
--- ParseAvitoAds/Form1.cs
+++ ParseAvitoAds/Form1.cs
@@ -12,6 +12,14 @@ namespace ParseAvitoAds
 			InitializeComponent();
 		}
 
+		public class Ads
+		{
+			string title = "";
+			string body = "";
+			string pic = "";
+			string phone = "";
+		}
+
 		public static string GetHtml(string url)
 		{
 			string fullurl = url;
@@ -20,6 +28,7 @@ namespace ParseAvitoAds
 			using (var webClient = new WebClient())
 			{
 				webClient.Encoding = System.Text.Encoding.UTF8;
+				webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12");
 				try
 				{
 					return webClient.DownloadString(fullurl);
@@ -35,6 +44,7 @@ namespace ParseAvitoAds
 		{
 			string startUrl = "https://www.avito.ru/moskva?radius=5&geoCoords=55.733939038966945%2C37.6406529148446";
 			string parseStr1 = "data-item-url=\"";
+			string avitoUrlStart = "https://www.avito.ru";
 
 			//loading main Avito page with recent ads
 			startButton.Text = "require main avito page..";
@@ -47,16 +57,29 @@ namespace ParseAvitoAds
 			string adsUrl = "init";
 			int ind1, ind2 = 1;
 			string[] urls = new string[100];
+			byte urlsCount = 0;
+			bool goParse = true;
 
 			//parsing ads urls from main page
-			while (adsUrl.Length > 0)
+			while (goParse)
 			{
 				ind1 = html1.IndexOf(parseStr1);
-				html1 = html1.Substring(ind1+ parseStr1.Length, (html1.Length - ind1 - parseStr1.Length));
-				ind2 = html1.IndexOf("\"");
-				if (ind1 > 0 && ind2 > 0) adsUrl = html1.Substring(0, ind2);
-				MessageBox.Show(adsUrl);
+				if (ind1 > 0)
+				{
+					html1 = html1.Substring(ind1 + parseStr1.Length, (html1.Length - ind1 - parseStr1.Length));
+					ind2 = html1.IndexOf("\"");
+					if (ind2 > 0)
+					{
+						adsUrl = avitoUrlStart + html1.Substring(0, ind2);
+						urls[urlsCount] = adsUrl;
+						urlsCount++;
+					}
+				}
+				else goParse = false;
 			}
+
+			File.WriteAllLines("html.txt", urls);
+			Environment.Exit(0);
 		}
 	}
 }
