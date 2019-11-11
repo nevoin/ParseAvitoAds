cb696ffae433c1a5fda5e57cf43b6891696a2e34
nevoin <fr_ord@mail.ru>
2019-11-11 18:50:35 +0300

    parse each ad

--- ParseAvitoAds/Form1.cs
+++ ParseAvitoAds/Form1.cs
@@ -2,14 +2,18 @@
 using System.Net;
 using System.Windows.Forms;
 using System.IO;
+using System.Threading;
 
 namespace ParseAvitoAds
 {
 	public partial class Form1 : Form
 	{
-		string startUrl = "https://www.avito.ru/moskva?radius=5&geoCoords=55.733939038966945%2C37.6406529148446";
-		string parseStr1 = "data-item-url=\"";
-		string avitoUrlBegin = "https://www.avito.ru";
+		const string startUrl = "https://www.avito.ru/moskva?radius=5&geoCoords=55.733939038966945%2C37.6406529148446";
+		const string parseAllUrls = "data-item-url=\"";
+		const string avitoUrlBegin = "https://www.avito.ru";
+		const string parseOneAdsTitle1 = "<div class=\"sticky-header-prop sticky-header-title\">";
+		const string parseOneAdsTitle2 = "</div>";
+
 		string[] urls = new string[100];
 		Ads[] parsedAds = new Ads[100];
 		byte urlsCount = 0;
@@ -21,7 +25,7 @@ namespace ParseAvitoAds
 
 		public class Ads
 		{
-			//string title = "";
+			public string title="";
 			//string body = "";
 			//string pic = "";
 			//string phone = "";
@@ -47,9 +51,32 @@ namespace ParseAvitoAds
 				}
 			}
 		}
-
-		public void ParseAllUrls()
+		
+		//parse each ads - title/body/phone/pic
+		public void ParseAllUrls(string[] urls, byte urlsCount)
 		{
+			Random r = new Random();
+			int ind1, ind2 = 0;
+			byte sleepAntiBan = 0;
+
+
+			//for (int i=0; i < urlsCount; i++)
+			for (int i = 0; i < 5; i++)
+			{
+				parsedAds[i] = new Ads();
+				sleepAntiBan = Convert.ToByte(r.Next(10));
+				startButton.Text = "Sleep " + sleepAntiBan.ToString() + " seconds";
+				Application.DoEvents();
+				Thread.Sleep(sleepAntiBan * 1000); //in seconds
+				string html1 = GetHtml(urls[i]);
+				ind1 = html1.IndexOf(parseOneAdsTitle1);
+				if (ind1>0)
+				{
+					html1 = html1.Substring(ind1 + parseOneAdsTitle1.Length);
+					ind2 = html1.IndexOf(parseOneAdsTitle2);
+					if (ind2 > 0) parsedAds[i].title = html1.Substring(0, ind2);
+				}
+			}
 		}
 
 		public void ParseMainPage(ref string [] urls, ref byte urlsCount)
@@ -68,10 +95,10 @@ namespace ParseAvitoAds
 			//parsing ads urls from main page
 			while (goParse)
 			{
-				ind1 = html1.IndexOf(parseStr1);
+				ind1 = html1.IndexOf(parseAllUrls);
 				if (ind1 > 0)
 				{
-					html1 = html1.Substring(ind1 + parseStr1.Length, (html1.Length - ind1 - parseStr1.Length));
+					html1 = html1.Substring(ind1 + parseAllUrls.Length, (html1.Length - ind1 - parseAllUrls.Length));
 					ind2 = html1.IndexOf("\"");
 					if (ind2 > 0)
 					{
@@ -88,9 +115,8 @@ namespace ParseAvitoAds
 		private void startButton_Click(object sender, EventArgs e)
 		{
 			ParseMainPage(ref urls, ref urlsCount);
-			File.WriteAllLines("allurls.txt", urls);
-
-			ParseAllUrls();
+			File.WriteAllText("allurls.txt",parsedAds.ToString());
+			ParseAllUrls(urls, urlsCount);
 
 			Environment.Exit(0);
 		}
